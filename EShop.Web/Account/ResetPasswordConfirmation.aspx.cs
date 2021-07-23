e238c3ce5aa4e6a121ca596b46640b3447dcde7d
javier <javier_aroca@hotmail.com>
2021-07-23 10:24:15 +0200

    Agregar archivos de proyecto.

---
+++ EShop.Web/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EShop.Web.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EShop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EShop.Web.Global" Language="C#" %>
